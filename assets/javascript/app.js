9d7005a9d93b9462d605ca62c10e45af153084bb
Kathleen Leung <kathleenleungky@gmail.com>
2019-07-12 15:51:09 -0700

    added images, more styling, completed html and some javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,137 @@
+// A list of Questions & Answers
+let questionsBank = [
+    // 1
+    {
+        question: "Who is the only Disney princess with a tattoo?",
+        answers: [
+            {
+                answer:"Moana",
+                correct: false,
+            },
+            {
+                answer:"Pocahontas",
+                correct: true,
+            },
+            {
+                answer:"Jasmine",
+                correct: false,
+            },
+            {
+                answer:"Merida",
+                correct: false,
+            }
+        ]
+    },
+    // 2
+    {
+        question: "Which Disney movie was the first animated film in history to be nominated for a Best Picture at the Academy Awards?",
+        answers:[
+            {
+                answer:"Beauty and the Beast",
+                correct: true,
+            },
+            {
+                answer:"Frozen",
+                correct: false,
+            },
+            {
+                answer:"Cinderella",
+                correct: false,
+            },
+            {
+                answer:"Hercules",
+                correct: false,
+            }
+        ]
+    },
+    // 3
+    {
+        question:"Who is the youngest Disney villain?",
+        answers:[
+            {
+                answer:"Frozen: Hans",
+                correct: true,
+            },
+            {
+                answer:"Beauty and the Beast: Gaston",
+                correct: false,
+            },
+            {
+                answer:"The Lion King: Scar",
+                correct: false,
+            },
+            {
+                answer:"Mulan: Shan Yu",
+                correct: false,
+            }
+        ]
+    },
+    // 4
+    {
+        question:"Who visited Arendelle from another movie to be guests at Elsa's coronation in Frozen?",
+        answers:[
+            {
+                answer:"Rapunzel & Flynn",
+                correct: true,
+            },
+            {
+                answer:"King Fergus & Queen Elinor",
+                correct: false,
+            },
+            {
+                answer:"Princess Fiona & Shrek",
+                correct: false,
+            },
+            {
+                answer:"Kristoff & Hans",
+                correct: false,
+            }
+        ]
+    },
+    // 5
+    {
+        question:"Who was considered to play the role of Princess Tiana in the The Princess and the Frog?",
+        answers:[
+            {
+                answer:"Beyonce",
+                correct: true,
+            },
+            {
+                answer:"Alicia Keys",
+                correct: false,
+            },
+            {
+                answer:"Mariah Carey",
+                correct: false,
+            },
+            {
+                answer:"Leona Lewis",
+                correct: false,
+            }
+        ]
+    },
+    // 6
+    {
+        question:"What was Walter Elias Disney's (the pioneer of Disney) favorite piece of animation?",
+        answers: [
+            {
+                answer:"Cinderella's dress transformation",
+                correct: true,
+            },
+            {
+                answer:"Ariel's tail transformation",
+                correct: false,
+            },
+            {
+                answer:"Hercules' rescue Meg from the Underworld",
+                correct: false,
+            },
+            {
+                answer:"Peter Pan's Flying Scene",
+                correct: false,
+            }
+        ]
+    }
+]
+
+// 
